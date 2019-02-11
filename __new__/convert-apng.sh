d313793fc587c9d6778e916d5a2aaea124ac405f
Ryan Parman <ryan@ryanparman.com>
2019-02-10 21:18:27 -0800

    WIP

--- __new__/convert-apng.sh
+++ __new__/convert-apng.sh
@@ -5,4 +5,17 @@ set -exo pipefail;
 # * https://engineering.giphy.com/how-to-make-gifs-with-ffmpeg/
 # * https://www.alecjacobson.com/weblog/?p=2601
 
-find . -type f -name '*.png' | xargs -P $(nproc) -I {} bash -c 'ff=$(basename -- "${1%.png}"); if [ ! -f "${ff}.gif" ]; then mkdir -p "/tmp/${ff}" && ffmpeg -i "$1" -filter_complex "[0:v] fps=12,scale=w=128:h=-1,split [a][b];[a] palettegen=stats_mode=single [p];[b][p] paletteuse=new=1" -y "/tmp/${ff}/%05d.png" && convert -dispose 2 "/tmp/${ff}/*.png" "${ff}.gif" && rm -Rf "/tmp/${ff}"; fi;' _ {} \;
+# find . -type f -name '*.png' | xargs -P $(nproc) -I {} bash -c 'ff=$(basename -- "${1%.png}"); if [ ! -f "${ff}.gif" ]; then mkdir -p "/tmp/${ff}" && ffmpeg -i "$1" -filter_complex "[0:v] fps=12,scale=w=128:h=-1,split [a][b];[a] palettegen=stats_mode=single [p];[b][p] paletteuse=new=1" -y "/tmp/${ff}/%05d.png" && convert -dispose 2 "/tmp/${ff}/*.png" "${ff}.gif" && rm -Rf "/tmp/${ff}"; fi;' _ {} \;
+find . -maxdepth 1 -type f -name '*.png' \
+    | xargs -P $(nproc) -I {} bash -c '
+        ff=$(basename -- "${1%.png}");
+        if [ ! -f "${ff}.gif" ]; then
+            mkdir -p "/tmp/${ff}" && \
+            ffmpeg \
+                -i "$1" \
+                -filter_complex "[0:v] fps=12,scale=w=128:h=-1,split [a][b];[a] palettegen=stats_mode=single [p];[b][p] paletteuse=new=1" \
+                -y "/tmp/${ff}/%05d.png" && \
+            convert -dispose 2 "/tmp/${ff}/*.png" "${ff}.gif" && \
+            rm -Rf "/tmp/${ff}";
+        fi;
+    ' _ {} \;
--- __new__/convert-mov.sh
+++ __new__/convert-mov.sh
@@ -1,3 +1,16 @@
 #! /usr/bin/env bash
 set -exo pipefail;
-find . -type f -name '*.mov' | xargs -P $(nproc) -I {} bash -c 'ff=$(basename -- "${1%.mov}"); if [ ! -f "${ff}.gif" ]; then mkdir -p "/tmp/${ff}" && ffmpeg -i "$1" -filter_complex "[0:v] fps=12,scale=w=128:h=-1,split [a][b];[a] palettegen=stats_mode=single [p];[b][p] paletteuse=new=1" -y "/tmp/${ff}/%05d.png" && convert -dispose 2 "/tmp/${ff}/*.png" "${ff}.gif" && rm -Rf "/tmp/${ff}"; fi;' _ {} \;
+
+find . -type f -name '*.mov' \
+    | xargs -P $(nproc) -I {} bash -c '
+        ff=$(basename -- "${1%.mov}");
+        if [ ! -f "${ff}.gif" ]; then
+            mkdir -p "/tmp/${ff}" && \
+            ffmpeg \
+                -i "$1" \
+                -filter_complex "[0:v] fps=12,scale=w=128:h=-1,split [a][b];[a] palettegen=stats_mode=single [p];[b][p] paletteuse=new=1" \
+                -y "/tmp/${ff}/%05d.png" && \
+            convert -dispose 2 "/tmp/${ff}/*.png" "${ff}.gif" && \
+            rm -Rf "/tmp/${ff}";
+        fi;
+    ' _ {} \;
